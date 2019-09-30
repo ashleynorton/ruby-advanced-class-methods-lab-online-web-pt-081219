9071b2cb0a444153d6e0ccf4c6f6d533280f7e88
Ashley Norton <nortonashleym@gmail.com>
2019-09-30 01:26:20 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -32,4 +32,12 @@ class Song
   def self.find_by_name(name)
     self.all.find {|song| song.name == name }
   end 
+  
+  def self.find_or_create_by_name(name)
+    if self.find_by_name == name 
+      return name 
+    else 
+      self.create_by_name
+    end 
+  end 
 end
